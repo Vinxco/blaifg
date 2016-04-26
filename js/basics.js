4baa0549d2a6fd8be7266d071d707c706d5d5f73
Vinxco <vicente.codon@gmail.com>
2016-04-26 21:13:10 +0200

    Segundo Branch
    pues he modificado html y JS, ahora comento a ver como va la cosa

--- js/basics.js
+++ js/basics.js
@@ -1,1 +1,6 @@
 alert("Simplemente un 'alert' de prueba");
+console.log("Por no enmerdar el código, voy a ir añadiendo cosas aquí");
+
+console.log("Este esle branch del branch, aunque esta si me aparece como yuna verdadera rama, la padre de esta me sale en la misma líne temporal");
+
+console.log("Esto lo pongo en un tercer branch, cosa que me parece excesiva, la verdad. Veremos al hacer un commit")
